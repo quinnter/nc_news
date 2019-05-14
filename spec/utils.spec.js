15ed259d968b58f1472688c04f665d6f6276b0f6
quinnter <lauraquinn@Lauras-MacBook-Air.local>
2019-05-14 12:23:54 +0100

    fixed formateDate function

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -6,7 +6,7 @@ const {
   formatDate
 } = require("../utils/formatting-functions");
 
-describe.only("createRef", () => {
+describe("createRef", () => {
     it("returns empty object when empty array is passed", () => {
         const input = [];
         const key = "";
@@ -27,8 +27,29 @@ describe.only("createRef", () => {
     })
 })
 
-describe(" ", () => {
-    it(" ", () => {
-
+describe.only("formatDate", () => {
+    it("returns an empty array when an empty array is passed", () => {
+        const input = [];
+        const actual = formatDate(input);
+        const expected = [];
+        expect(actual).to.eql(expected);
+    })
+    it("returns an array of objects where the created_at key value is parsed", () => {
+        const input = [{
+            title: 'Moustache',
+            topic: 'mitch',
+            author: 'butter_bridge',
+            body: 'Have you seen the size of that thing?',
+            created_at: 154700514171,
+          }];
+        const actual = formatDate(input);
+        const expected = [{
+            title: 'Moustache',
+            topic: 'mitch',
+            author: 'butter_bridge',
+            body: 'Have you seen the size of that thing?',
+            created_at: 'Tue, 26 Nov 1974 12:21:54 GMT',
+          }];
+        expect(actual).to.eql(expected);
     })
 })
