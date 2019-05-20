add50face2d46d484e3a163d950928838961e996
quinnter <lauraquinn@Lauras-MacBook-Air.local>
2019-05-20 09:11:51 +0100

    refactored code to pass PATCH 404

--- models/commentsModel.js
+++ models/commentsModel.js
@@ -7,7 +7,7 @@ exports.updateCommentVotes = (comment_id, inc_votes) => {
     .increment({'votes': inc_votes})
     .returning('*')
     .then(([comment]) => {
-      if (comment.length === 0) return Promise.reject({ code: 404})
+      if (!comment) return Promise.reject({ code: 404})
       else return comment
     })
 }
