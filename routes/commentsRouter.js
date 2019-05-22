0363aa9c4f3d793ae95f7e2c04a161679d31606f
quinnter <lauraquinn@Lauras-MacBook-Air.local>
2019-05-22 13:56:40 +0100

    added methodNotAllowed function to user and topic route

--- routes/commentsRouter.js
+++ routes/commentsRouter.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const commentsRouter = express.Router()
 const { patchCommentById, deleteCommentById } = require("../controllers/commentsController")
+const { methodNotAllowed } = require("../errors/index")
 
 commentsRouter.route("/:comment_id")
 .patch(patchCommentById)
 .delete(deleteCommentById)
+.all(methodNotAllowed)
 
 module.exports = commentsRouter
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const usersRouter = express.Router()
 const { getUsers, getUser } = require("../controllers/usersController")
+const { methodNotAllowed } = require("../errors/index")
 
 usersRouter.route("/").get(getUsers)
-usersRouter.route("/:username").get(getUser)
+usersRouter.route("/:username").get(getUser).all(methodNotAllowed)
 
 module.exports = usersRouter
