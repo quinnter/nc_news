e41918d5cbccd02053436ef18e7b96165979ac8c
quinnter <lauraquinn@Lauras-MacBook-Air.local>
2019-06-21 12:14:09 +0100

    removed getEndpoints from api route

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,12 @@
 const apiRouter = require('express').Router();
 const { methodNotAllowed } = require('../errors');
-const  topicsRouter  = require("./topicsRouter")
+const topicsRouter = require("./topicsRouter")
 const articlesRouter = require("./articlesRouter")
 const usersRouter = require("./usersRouter")
 const commentsRouter = require("./commentsRouter")
-const { getEndPoints } = require("../controllers/apiController")
 
 apiRouter
   .route('/')
-  .get(getEndPoints)
   .all(methodNotAllowed);
 
 apiRouter.use("/topics", topicsRouter)
